2ddba13e94b411ef50584228697edae3722bc589
Aleksey Avdeev <avdeev@altell.ru>
2014-03-27 13:12:02 +0400

    Only cvsimport.sh

--- geturls.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-((wget http://download.virtualbox.org/virtualbox/ -q -O -|sed -rn '/^<A HREF="/s@^<A HREF="([0-9]+\.[0-9]+\.[0-9]+[^/]*/)".*$@http://download.virtualbox.org/virtualbox/\1@p' ) | xargs wget -q -O - )|sed -rn '/^<A HREF="VirtualBox-/s@^<A HREF="(VirtualBox-([0-9]+\.[0-9]+\.[0-9]+(_(BETA|RC)[0-9]+)?)([_-]OSE)?\.tar\.bz2)".*$@http://download.virtualbox.org/virtualbox/\2/\1@p'
